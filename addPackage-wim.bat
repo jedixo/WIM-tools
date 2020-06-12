3fc2595b2166a2c87f04e3f0cbbf5e2b4e67140c
jake Dixon <jake.dixon@my.jcu.edu.au>
2020-06-12 13:05:29 +1000

    added status indicators and information text

--- addPackage-wim.bat
+++ addPackage-wim.bat
@@ -21,16 +21,21 @@ Powershell.exe write-host -foregroundcolor red ERROR: Please run as an Administr
 
 :start
 cls
-echo.
 if exist %~dp1mnt (
+cls
+Powershell.exe write-host -foregroundcolor yellow PROC: Previously mounted image found... unmounting and discarding changes...
 dism /unmount-image /mountdir:%~dp1mnt\ /discard > %~dp1\log.txt
 RD %~dp1mnt /S /Q)
 md %~dp1mnt
+cls
+Powershell.exe write-host -foregroundcolor yellow PROC: Mounting .WIM Image...
 dism /mount-image /imagefile:%1 /index:1 /mountdir:%~dp1mnt\ > %~dp1\log.txt
 cls
 
 :selectPackage
-set /p PackageDir=Drag an pacakge file onto this window to apply package, or press x to Exit... 
+cls
+Powershell.exe write-host -foregroundcolor cyan Drag an pacakge file onto this window to apply package, or press x to Exit... 
+set /p PackageDir= 
 if %PackageDir% EQU x goto exit
 echo %PackageDir%|find ".cab" >nul
 if errorlevel 1 (echo.) else goto addPackage
@@ -39,11 +44,14 @@ if errorlevel 1 (echo.) else goto addPackage
 goto selectPackage
 
 :addPackage
-dism /add-package /image:%~dp1mnt\ /packagepath:%PackageDir% > %~dp1\log.txt
+Powershell.exe write-host -foregroundcolor yellow PROC: Adding package to mounted image...
+dism /add-package /image:%~dp1mnt\ /packagepath:%PackageDir% >> %~dp1\log.txt
 goto selectPackage
 
 :exit
-dism /unmount-image /mountdir:%~dp1mnt\ /commit > %~dp1\log.txt
+cls
+Powershell.exe write-host -foregroundcolor yellow PROC: Unmounting Image and saving changes...
+dism /unmount-image /mountdir:%~dp1mnt\ /commit >> %~dp1\log.txt
 if exist %~dp1mnt (RD %~dp1mnt /S /Q)
-Powershell.exe write-host -foregroundcolor cyan Press any key to continue . . .
+Powershell.exe write-host -foregroundcolor cyan DONE: Press any key to exit . . .
 pause>nul
